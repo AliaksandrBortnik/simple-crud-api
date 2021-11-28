ece374af9e1b33a185ea3bccd546fbfa85f71b3e
Alex Bortnik <bortnik.ar@gmail.com>
2021-11-28 14:27:19 +0300

    Erased all test data from "in-memory" db

--- src/db.js
+++ src/db.js
@@ -1,19 +1,3 @@
 module.exports = {
-  persons: [
-    {
-      id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
-      name: 'Bob',
-      age: 27,
-      hobbies: [
-        'programming',
-        'nodejs'
-      ]
-    },
-    {
-      id: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
-      name: 'Martin',
-      age: 22,
-      hobbies: []
-    }
-  ]
+  persons: []
 };
